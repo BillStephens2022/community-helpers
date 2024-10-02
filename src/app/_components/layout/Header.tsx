842401550d94e1b2ba94334bdb16160bf7cb10c7
BillStephens2022 <two4onebill@yahoo.com>
2024-10-02 07:02:40 -0400

    improved responsiveness of header and navbar on phone size screen

--- src/app/_components/layout/Header.tsx
+++ src/app/_components/layout/Header.tsx
@@ -5,9 +5,11 @@ import Link from "next/link";
 export default function Header() {
   return (
     <div className={styles.header_div}>
-      <h1 className={styles.header_h1}>
-        <Link href="/">Community Helpers</Link>
-      </h1>
+      
+      <Link href="/" className={styles.header_link}>
+        <h1 className={styles.header_h1}>Community Helpers</h1>
+      </Link>
+  
       <Navigation />
     </div>
   );
