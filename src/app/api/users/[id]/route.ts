5216f71d1db3df40730464eca530ecc11f7bcec5
BillStephens2022 <two4onebill@yahoo.com>
2024-10-06 10:47:03 -0400

    added route handler for updating a user's profile

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -38,3 +38,42 @@ export async function GET(
     );
   }
 }
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  console.log("Update user route hit!");
+  const { id } = params;
+
+  try {
+    await dbConnect();
+
+    // Parse the request body to get updated user data
+    const updatedData = await req.json();
+
+    // Find the user by ID and update their information
+    const updatedUser = await User.findByIdAndUpdate(id, updatedData, {
+      new: true, // Return the updated document
+      runValidators: true, // Validate the update against the model
+    });
+
+    if (!updatedUser) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(
+      {
+        message: "User updated successfully",
+        user: updatedUser,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Internal server error." },
+      { status: 500 }
+    );
+  }
+}
