d81bf66766ba8f90a847a42958e3176a0939d418
BillStephens2022 <two4onebill@yahoo.com>
2024-10-08 17:04:22 -0400

    updating spacing/alignment on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,36 +5,36 @@ import chlogo from "./images/chlogo.jpeg";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <div className={styles.home_left}>
-          <Image
-            className={styles.logo}
-            src={chlogo}
-            alt="Next.js logo"
-            width={500}
-            height={500}
-            priority
-          />
-          <Link href="/login" className={styles.get_started_link}>Let&apos;s Go!</Link>
-        </div>
-        <div className={styles.home_right}>
-          <h3 className={styles.home_h3}>Need a job done?</h3>
-          <h4 className={styles.home_h4}>
-            Your community has the skills you need!
-          </h4>
-          <ul className={styles.home_ul}>
-            <li className={styles.home_li}>Handyman Services</li>
-            <li className={styles.home_li}>Yard Work</li>
-            <li className={styles.home_li}>House Cleaning</li>
-            <li className={styles.home_li}>Child Care</li>
-            <li className={styles.home_li}>Senior Care</li>
-            <li className={styles.home_li}>Auto Repair</li>
-            <li className={styles.home_li}>Computer Repair</li>
-            <li className={styles.home_li}>Dog Walking / Pet Sitting</li>
-          </ul>
-        </div>
-      </main>
-    </div>
+    <main className={styles.main}>
+      <div className={styles.home_left}>
+        <Image
+          className={styles.logo}
+          src={chlogo}
+          alt="Next.js logo"
+          width={425}
+          height={425}
+          priority
+        />
+        <Link href="/login" className={styles.get_started_link}>
+          Let&apos;s Go!
+        </Link>
+      </div>
+      <div className={styles.home_right}>
+        <h3 className={styles.home_h3}>Need a job done?</h3>
+        <h4 className={styles.home_h4}>
+          Your community has the skills you need!
+        </h4>
+        <ul className={styles.home_ul}>
+          <li className={styles.home_li}>Handyman Services</li>
+          <li className={styles.home_li}>Yard Work</li>
+          <li className={styles.home_li}>House Cleaning</li>
+          <li className={styles.home_li}>Child Care</li>
+          <li className={styles.home_li}>Senior Care</li>
+          <li className={styles.home_li}>Auto Repair</li>
+          <li className={styles.home_li}>Technology</li>
+          <li className={styles.home_li}>Dog Walking / Pet Sitting</li>
+        </ul>
+      </div>
+    </main>
   );
 }
