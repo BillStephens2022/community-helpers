09356db7ec231b83c00a68a878c2603abd6fd722
BillStephens2022 <two4onebill@yahoo.com>
2024-11-28 08:39:06 -0500

    restyled navbar

--- src/app/_components/layout/Navigation.tsx
+++ src/app/_components/layout/Navigation.tsx
@@ -1,38 +1,65 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
-import styles from "./navigation.module.css";
+import { usePathname } from "next/navigation";
 import { signOut, useSession } from "next-auth/react";
+import styles from "./navigation.module.css";
 
 
 export default function Navigation() {
   const {data: session } = useSession();
+  const [isChecked, setIsChecked] = useState(false);
+  const pathname = usePathname();
+
+  const handleLinkClick = () => {
+    setIsChecked(false);
+  };
 
   const handleLogoff = async () => {
     await signOut({ redirect: true, callbackUrl: "/" }); // Redirect to homepage after logging out
   };
 
   return (
-    <nav className={styles.navbar}>
-      <ul className={styles.navbar_ul}>
-        <li>
-          <Link href="/about" className={styles.navbar_link}>
+    <>
+    <div className={styles.navbar}>
+      <input
+        type="checkbox"
+        className={styles.nav_checkbox}
+        id="navi-toggle"
+        checked={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+      />
+      <label htmlFor="navi-toggle" className={styles.nav_button}>
+        <span className={styles.nav_icon}>&nbsp;</span>
+      </label>
+      <div className={styles.nav_background}></div>
+    <nav className={styles.navbar_nav}>
+      <ul className={styles.nav_items}>
+        <li className={styles.nav_item}>
+          <Link href="/about" className={`${styles.nav_item} ${styles.nav_link} ${
+                  pathname === "/about" ? styles.active : ""
+                }`} onClick={handleLinkClick}>
             About
           </Link>
         </li>
         {session ? (
           <>
-            <li>
-              <Link href="/profile" className={styles.navbar_link}>
+            <li className={styles.nav_item}>
+              <Link href="/profile" className={`${styles.nav_item} ${styles.nav_link} ${
+                  pathname === "/profile" ? styles.active : ""
+                }`} onClick={handleLinkClick}>
                 Profile
               </Link>
             </li>
-            <li>
-              <Link href="/community" className={styles.navbar_link}>
+            <li className={styles.nav_item}>
+              <Link href="/community" className={`${styles.nav_item} ${styles.nav_link} ${
+                  pathname === "/community" ? styles.active : ""
+                }`} onClick={handleLinkClick}>
                 Community
               </Link>
             </li>
-            <li>
+            <li className={styles.nav_logout_button}>
               <button
                 type="button"
                 onClick={handleLogoff}
@@ -51,5 +78,7 @@ export default function Navigation() {
         )}
       </ul>
     </nav>
+    </div>
+    </>
   );
 }
